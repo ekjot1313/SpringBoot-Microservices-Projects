955a5da0e6ee487d3ee008a1430161802b6b7862
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 02:34:54 -0400

    added user-department-discovery-server Spring initializr

---
+++ user-department-discovery-server/src/main/java/com/example/userdepartmentdiscoveryserver/UserDepartmentDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmentdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepartmentDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ user-department-discovery-server/src/test/java/com/example/userdepartmentdiscoveryserver/UserDepartmentDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmentdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepartmentDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
