2f1a50688a201784f6e37cd475c9efa35af03674
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 00:25:26 -0400

    added DepartmentService.

---
+++ department-service/src/main/java/com/example/department/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package com.example.department.service;
+
+import com.example.department.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+}
