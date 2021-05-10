8cd6ea7960825e626187ba256e3a3daa19f8880f
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 13:42:12 -0400

    added user-department-config-server from Spring Initializr

---
+++ user-department-config-server/src/main/java/com/example/userdepartmentconfigserver/UserDepartmentConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmentconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepartmentConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentConfigServerApplication.class, args);
+	}
+
+}
---
+++ user-department-config-server/src/test/java/com/example/userdepartmentconfigserver/UserDepartmentConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmentconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepartmentConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
