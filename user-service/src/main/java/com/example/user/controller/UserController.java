5d5f330b8ce3e9f06edb4d7d2cbab5e7b4b10a4e
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 02:13:14 -0400

    added ResponseTemplateVO and getUserWithDepartment in UserController

--- user-service/src/main/java/com/example/user/controller/UserController.java
+++ user-service/src/main/java/com/example/user/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.example.user.controller;
 
+import com.example.user.VO.ResponseTemplateVO;
 import com.example.user.entity.User;
 import com.example.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,5 +18,11 @@ public class UserController {
     public User saveUser(@RequestBody User user){
         log.info("Inside saveUser method of UserController");
         return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
+        log.info("Inside the getUserWithDepartment method of UserController");
+        return userService.getUserWithDepartment(userId);
     }
 }
--- user-service/src/main/java/com/example/user/service/UserService.java
+++ user-service/src/main/java/com/example/user/service/UserService.java
@@ -1,19 +1,39 @@
 package com.example.user.service;
 
+import com.example.user.VO.Department;
+import com.example.user.VO.ResponseTemplateVO;
 import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public User saveUser(User user) {
         log.info("Inside saveUser method of UserService.");
         return userRepository.save(user);
+    }
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("Inside the getUserWithDepartment method of UserService");
+        ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
+        User user = userRepository.findByUserId(userId);
+
+        Department department =
+                restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId(),
+                        Department.class);
+        responseTemplateVO.setUser(user);
+        responseTemplateVO.setDepartment(department);
+        return responseTemplateVO;
     }
 }
