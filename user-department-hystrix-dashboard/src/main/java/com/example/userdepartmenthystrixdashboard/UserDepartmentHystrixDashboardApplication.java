708925977b8d95f52ecbf13675632ec9e79d1e2e
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 12:39:36 -0400

    added user-department-hystrix-dashboard from Spring Initializr

---
+++ user-department-hystrix-dashboard/src/main/java/com/example/userdepartmenthystrixdashboard/UserDepartmentHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmenthystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepartmentHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ user-department-hystrix-dashboard/src/test/java/com/example/userdepartmenthystrixdashboard/UserDepartmentHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdepartmenthystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepartmentHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
