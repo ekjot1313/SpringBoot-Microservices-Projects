fe80109b3c2bdab68f022e0d0f48407c397e8009
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 01:50:20 -0400

    added UserController with saveUser

--- user-service/src/main/java/com/example/user/service/UserService.java
+++ user-service/src/main/java/com/example/user/service/UserService.java
@@ -1,11 +1,19 @@
 package com.example.user.service;
 
+import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        log.info("Inside saveUser method of UserService.");
+        return userRepository.save(user);
+    }
 }
