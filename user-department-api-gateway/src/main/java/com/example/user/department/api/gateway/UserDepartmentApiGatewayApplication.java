b338ea0cabe712ae38070fe3bc62f28b323572ef
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 11:31:33 -0400

    added user-department-api-gateway from Spring Initializr

---
+++ user-department-api-gateway/src/main/java/com/example/user/department/api/gateway/UserDepartmentApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.user.department.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepartmentApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentApiGatewayApplication.class, args);
+	}
+
+}
