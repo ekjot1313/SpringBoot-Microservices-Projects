c4998e40425d8b740ee29f373c8fe909360a6d35
Ekjot Singh <ekjotsingh00776@gmail.com>
2021-05-10 13:47:24 -0400

    added application.yml in user-department-config-server

--- user-department-config-server/src/main/java/com/example/userdepartmentconfigserver/UserDepartmentConfigServerApplication.java
+++ user-department-config-server/src/main/java/com/example/userdepartmentconfigserver/UserDepartmentConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.example.userdepartmentconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class UserDepartmentConfigServerApplication {
 
 	public static void main(String[] args) {
